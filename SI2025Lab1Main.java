fae5dc43e7ce4885c8635390b6fd5c61baa391d9
Filip Kuzmanovski <kuzmanovskifilip2004@gmail.com>
2025-04-02 02:52:23 +0200

    Added find all completed tasks method

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -70,7 +70,13 @@ class TaskManager {
     // 2. Find all completed tasks
     public List<Task> getCompletedTasks() {
         // TODO: Implement logic to return completed tasks
-        return new ArrayList<>();
+        List<Task> completedTasks=new ArrayList<>();
+        for (Task task : tasks){
+            if (task.isCompleted()){
+                completedTasks.add(task);
+            }
+        }
+        return completedTasks;
     }
 
     // 3. List tasks sorted by name
@@ -122,5 +128,11 @@ public class SI2025Lab1Main {
         // MISSING: Calls to the new methods that will be implemented
 
         manager.printTasks();
+
+        List<Task> completed=manager.getCompletedTasks();
+        System.out.println("Completed tasks:");
+        for (Task task : completed){
+            System.out.println(task);
+        }
     }
 }
